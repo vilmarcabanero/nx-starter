b9bff03e388a46b6437ce1a10816a60fe7b9fda0
Vilmar Cabañero <vilmarcabanero@gmail.com>
2025-07-18 17:26:31 +0800

    refactor: rename vite config usage for libs to vitest config

--- nx-starter/libs/domain-core/vitest.config.ts
+++ nx-starter/libs/domain-core/vitest.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../node_modules/.vite/shared-domain',
-  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
+  resolve: {
+    alias: {
+      '@nx-starter/shared-utils': path.resolve(__dirname, '../utils-core/src/index.ts'),
+    },
+  },
   test: {
     watch: false,
     globals: true,
